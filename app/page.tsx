842d72738955e7180b37f3475341fe7181c704cc
ctphiri <ctphiri1@gmail.com>
2025-07-02 13:05:20 -0400

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import TaxiBookingForm from "../components/booking-form"
+
+export default function Page() {
+  return (
+    <main className="min-h-screen bg-gray-50 py-8">
+      <TaxiBookingForm />
+    </main>
+  )
+}
